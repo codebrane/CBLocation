5460bb8a6f2e8074d968ceae4f5c2e0e432d20c1
Alistair Young <alistair@codebrane.com>
2011-02-03 15:57:12 +0000

    Tidied up debug stuff

--- CBLocationUtils.m
+++ CBLocationUtils.m
@@ -39,11 +39,8 @@
     self.lat0 = [self degreesToRadians:49];
     self.lon0 = [self degreesToRadians:-2];
     self.e2 = 1 - (self.b*self.b)/(self.a*self.a);
-//    self.n = ((self.a - self.b) / (self.a + self.b));
     self.n = (self.a-self.b)/(self.a+self.b);
-//    self.n2 = (self.n * self.n);
     self.n2 = self.n*self.n;
-//    self.n3 = (self.n * self.n * self.n);
     self.n3 = self.n*self.n*self.n;
   }
   
@@ -92,72 +89,49 @@
 
 
 // transverse radius of curvature
--(float)nu:(float)latitude { // OK
-  // nu = 6390092.714836496
-  NSLog(@"nu = %f", self.a*self.F0/sqrt(1-self.e2*[self sinLatitude:[self degreesToRadians:latitude]]*[self sinLatitude:[self degreesToRadians:latitude]]));
+-(float)nu:(float)latitude {
   return self.a*self.F0/sqrt(1-self.e2*[self sinLatitude:[self degreesToRadians:latitude]]*[self sinLatitude:[self degreesToRadians:latitude]]);
 }
 
 // meridional radius of curvature
--(float)rho:(float)latitude { // OK
-  // rho = 6377517.009942445
-  NSLog(@"rho = %f", self.a*self.F0*(1-self.e2)/pow(1-self.e2*[self sinLatitude:[self degreesToRadians:latitude]]*[self sinLatitude:[self degreesToRadians:latitude]], 1.5));
+-(float)rho:(float)latitude {
   return self.a*self.F0*(1-self.e2)/pow(1-self.e2*[self sinLatitude:[self degreesToRadians:latitude]]*[self sinLatitude:[self degreesToRadians:latitude]], 1.5);
 }
 
--(float)eta2:(float)latitude { // OK
-  // eta2 = 0.0019718810431152978
-  NSLog(@"eta2 = %f", [self nu:latitude]/[self rho:latitude]-1);
+-(float)eta2:(float)latitude {
   return [self nu:latitude]/[self rho:latitude]-1;
 }
 
--(float)Ma:(float)latitude { // OK
-  // Ma = 0.14314717592879525
-  NSLog(@"Ma = %f", (1 + self.n + (5/4)*self.n2 + (5/4)*self.n3) * ([self degreesToRadians:latitude]-lat0));
+-(float)Ma:(float)latitude {
   return (1 + self.n + (5/4)*self.n2 + (5/4)*self.n3) * ([self degreesToRadians:latitude]-lat0);
 }
 
--(float)Mb:(float)latitude { // OK
-  // Mb = -0.00019964323852434584
-  NSLog(@"Mb = %f", ((3 * self.n) + (3 * self.n * self.n) + ((21/8) * self.n3)) * [self sinLatitude:([self degreesToRadians:latitude] - self.lat0)] * [self cosLatitude:([self degreesToRadians:latitude] + self.lat0)]);
+-(float)Mb:(float)latitude {
   return ((3 * self.n) + (3 * self.n * self.n) + ((21/8) * self.n3)) * [self sinLatitude:([self degreesToRadians:latitude] - self.lat0)] * [self cosLatitude:([self degreesToRadians:latitude] + self.lat0)];
 }
 
--(float)Mc:(float)latitude { // OK
-  // Mc = -0.0000012520341642447784
-  NSLog(@"Mc = %f", ((15/8 * self.n2) + (15/8 * self.n3)) * [self sinLatitude:(2 * ([self degreesToRadians:latitude] - self.lat0))] * [self cosLatitude:(2 * ([self degreesToRadians:latitude] + self.lat0))]);
+-(float)Mc:(float)latitude {
   return ((15/8 * self.n2) + (15/8 * self.n3)) * [self sinLatitude:(2 * ([self degreesToRadians:latitude] - self.lat0))] * [self cosLatitude:(2 * ([self degreesToRadians:latitude] + self.lat0))];
 }
 
--(float)Md:(float)latitude { // NOT OK 0.00000000
-  // Md = 2.129074482992922e-9
-  NSLog(@"Md = %f", (35/24)*self.n3 * [self sinLatitude:(3*([self degreesToRadians:latitude]-self.lat0))] * [self cosLatitude:(3*([self degreesToRadians:latitude]+self.lat0))]);
+-(float)Md:(float)latitude {
   return (35/24)*self.n3 * [self sinLatitude:(3*([self degreesToRadians:latitude]-self.lat0))] * [self cosLatitude:(3*([self degreesToRadians:latitude]+self.lat0))];
 }
 
 // meridional arc
--(float)M:(float)latitude { // OK
-  // M = 910777.9402781258
-  NSLog(@"M = %f", self.b * self.F0 * ([self Ma:latitude] - [self Mb:latitude] + [self Mc:latitude] - [self Md:latitude]));
+-(float)M:(float)latitude {
   return self.b * self.F0 * ([self Ma:latitude] - [self Mb:latitude] + [self Mc:latitude] - [self Md:latitude]);
 }
 
--(float)I:(float)latitude { // OK
-  // I = 810777.9402781258
-  NSLog(@"I = %f",([self M:latitude] + self.N0));
+-(float)I:(float)latitude {
   return ([self M:latitude] + self.N0);
 }
 
--(float)II:(float)latitude { // OK
-  // II = 1455114.5656239567
-  NSLog(@"II = %f", ([self nu:latitude]/2)*[self sinLatitude:[self degreesToRadians:latitude]]*[self cosLatitude:[self degreesToRadians:latitude]]);
+-(float)II:(float)latitude {
   return ([self nu:latitude] / 2) * [self sinLatitude:[self degreesToRadians:latitude]] * [self cosLatitude:[self degreesToRadians:latitude]];
 }
 
--(float)III:(float)latitude { // OK
-  // III = 93011.21410939026
-  NSLog(@"III = %f", ([self nu:latitude]/24)*[self sinLatitude:[self degreesToRadians:latitude]]*[self cos3Latitude:[self degreesToRadians:latitude]]*(5-[self tan2Latitude:[self degreesToRadians:latitude]]+9*[self eta2:latitude]));
-  
+-(float)III:(float)latitude {
   return ([self nu:latitude] / 24) *
           [self sinLatitude:[self degreesToRadians:latitude]] *
           [self cos3Latitude:[self degreesToRadians:latitude]] *
@@ -165,29 +139,19 @@
            9 * [self eta2:latitude]);
 }
 
--(float)IIIA:(float)latitude { // OK
-  // IIIA = -25349.233056787063
-  NSLog(@"IIIA = %f", ([self nu:latitude]/720)*[self sinLatitude:[self degreesToRadians:latitude]]*[self cos5Latitude:[self degreesToRadians:latitude]]*(61-58*[self tan2Latitude:[self degreesToRadians:latitude]]+[self tan4Latitude:[self degreesToRadians:latitude]]));
-  
+-(float)IIIA:(float)latitude {
   return ([self nu:latitude]/720)*[self sinLatitude:[self degreesToRadians:latitude]]*[self cos5Latitude:[self degreesToRadians:latitude]]*(61-58*[self tan2Latitude:[self degreesToRadians:latitude]]+[self tan4Latitude:[self degreesToRadians:latitude]]);
 }
 
--(float)IV:(float)latitude { // OK
-  // IV = 3462690.5742993304
-  NSLog(@"IV = %f", [self nu:latitude] * [self cosLatitude:[self degreesToRadians:latitude]]);
+-(float)IV:(float)latitude {
   return [self nu:latitude] * [self cosLatitude:[self degreesToRadians:latitude]];
 }
 
--(float)V:(float)latitude { // OK
-  // V = -237854.47036671816
-  NSLog(@"V = %f", ([self nu:latitude]/6)*[self cos3Latitude:[self degreesToRadians:latitude]]*([self nu:latitude]/[self rho:latitude]-[self tan2Latitude:[self degreesToRadians:latitude]]));
-  
+-(float)V:(float)latitude {
   return ([self nu:latitude]/6)*[self cos3Latitude:[self degreesToRadians:latitude]]*([self nu:latitude]/[self rho:latitude]-[self tan2Latitude:[self degreesToRadians:latitude]]);
 }
 
--(float)VI:(float)latitude { // OK
-  // VI = -81510.18107681091
-  NSLog(@"VI = %f", ([self nu:latitude]/120) * [self cos5Latitude:[self degreesToRadians:latitude]] * (5 - 18*[self tan2Latitude:[self degreesToRadians:latitude]] + [self tan4Latitude:[self degreesToRadians:latitude]] + 14*[self eta2:latitude] - 58*[self tan2Latitude:[self degreesToRadians:latitude]]*[self eta2:latitude]));
+-(float)VI:(float)latitude {
   return ([self nu:latitude]/120) * [self cos5Latitude:[self degreesToRadians:latitude]] * (5 - 18*[self tan2Latitude:[self degreesToRadians:latitude]] + [self tan4Latitude:[self degreesToRadians:latitude]] + 14*[self eta2:latitude] - 58*[self tan2Latitude:[self degreesToRadians:latitude]]*[self eta2:latitude]);
 }
 
@@ -215,31 +179,27 @@
   return ([self dLon5:longitude] * [self dLon:longitude]);
 }
 
--(float)N:(float)latitude longitude:(float)longitude { // OK
-  // N = 817514.8991987555
-  NSLog(@"N = %f",  [self I:latitude] + [self II:latitude]*[self dLon2:longitude] + [self III:latitude]*[self dLon4:longitude] + [self IIIA:latitude]*[self dLon6:longitude]);
+-(float)N:(float)latitude longitude:(float)longitude {
   return [self I:latitude] + [self II:latitude]*[self dLon2:longitude] + [self III:latitude]*[self dLon4:longitude] + [self IIIA:latitude]*[self dLon6:longitude];
 }
 
--(float)E:(float)latitude longitude:(float)longitude { // OK
-  // E = 164498.0164299632
-  NSLog(@"E = %f", self.E0 + [self IV:latitude]*[self dLon:longitude] + [self V:latitude]*[self dLon3:longitude] + [self VI:latitude]*[self dLon5:longitude]);
+-(float)E:(float)latitude longitude:(float)longitude {
   return self.E0 + [self IV:latitude]*[self dLon:longitude] + [self V:latitude]*[self dLon3:longitude] + [self VI:latitude]*[self dLon5:longitude];
 }
 
 // convert numeric grid reference (in metres) to standard-form grid ref
 -(NSString *)gridrefNumToLet:(float)E N:(float)N digits:(int)digits {
   // get the 100km-grid indices
-  int e100k = floor(E / 100000); // e100k = 1 OK
-  int n100k = floor(N / 100000); // n100k = 8 OK
+  int e100k = floor(E / 100000);
+  int n100k = floor(N / 100000);
   
   if (e100k < 0 || e100k > 6 || n100k < 0 || n100k > 12) {
     return [[[NSString alloc] initWithString:@"out of range"] autorelease];
   }
   
   // translate those into numeric equivalents of the grid letters
-  int l1 = (19-n100k) - (19-n100k)%5 + floor((e100k+10)/5); // l1 = 12 OK
-  int l2 = (19-n100k)*5%25 + e100k%5; // l2 = 6 OK
+  int l1 = (19-n100k) - (19-n100k)%5 + floor((e100k+10)/5);
+  int l2 = (19-n100k)*5%25 + e100k%5;
   
   // compensate for skipped 'I' and build grid letter-pairs
   if (l1 > 7) l1++;
@@ -250,8 +210,8 @@
   NSString *firstGridLetter = [[NSString alloc] initWithBytes:(char *)&l1 length:1 encoding:NSUTF8StringEncoding];
   NSString *secondGridLetter = [[NSString alloc] initWithBytes:(char *)&l2 length:1 encoding:NSUTF8StringEncoding];
   
-  int easting = floor((((int)E)%100000) / pow(10, 5-digits/2)); // e = 6449 OK
-  int northing = floor((((int)N)%100000) / pow(10, 5-digits/2)); // n = 1751 OK
+  int easting = floor((((int)E)%100000) / pow(10, 5-digits/2));
+  int northing = floor((((int)N)%100000) / pow(10, 5-digits/2));
   
   NSString* gridRef = [[NSString stringWithFormat:@"%@%@ %d %d", firstGridLetter, secondGridLetter, easting, northing] autorelease];
   
@@ -260,17 +220,5 @@
   
   return gridRef;
 }
-
-
-
-
-/*
--(NationalGridTrueOrigin*)getNationalGridTrueOrigin {
-  NationalGridTrueOrigin *origin = malloc(sizeof(NationalGridTrueOrigin));
-  origin->lat0 = [self degreesToRadians:49];
-  origin->lon0 = [self degreesToRadians:-2];
-  return origin;
-}
- */
 
 @end
