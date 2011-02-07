17cae437d3b0b034daf169b8245d5f7d96e2b1e8
Alistair Young <alistair@codebrane.com>
2011-02-07 13:51:19 +0000

    Latitude/longitude encapsulation

---
+++ CBLatLon.h
@@ -0,0 +1,26 @@
+// "The contents of this file are subject to the Mozilla Public License
+// Version 1.1 (the "License"); you may not use this file except in
+// compliance with the License. You may obtain a copy of the License at
+// http://www.mozilla.org/MPL/
+//
+// Software distributed under the License is distributed on an "AS IS"
+// basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+// License for the specific language governing rights and limitations
+// under the License.
+//
+// The Original Code is CBLocation https://github.com/codebrane/CBLocation
+//
+// The Initial Developer of the Original Code is Alistair Young alistair@codebrane.com
+// All Rights Reserved.
+
+#import <Foundation/Foundation.h>
+
+@interface CBLatLon : NSObject {
+	double latitude;
+	double longitude;
+}
+
+@property double latitude;
+@property double longitude;
+
+@end
