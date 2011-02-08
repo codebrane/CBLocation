3b2f4cfdf7c6333e5ed153181f063541f7c96bf6
Alistair Young <alistair@codebrane.com>
2011-02-08 14:08:01 +0000

    Added doxygen comments

--- CBLocation.h
+++ CBLocation.h
@@ -13,6 +13,14 @@
 // The Initial Developer of the Original Code is Alistair Young alistair@codebrane.com
 // All Rights Reserved.
 
+/**
+ * CBLocation.
+ *
+ * Location utilities for iOS devices
+ *
+ * @author alistair
+ */
+
 #import <Foundation/Foundation.h>
 
 #import "CBLatLon.h"
@@ -80,8 +88,45 @@ typedef struct {
 @property double n2;
 @property double n3;
 
+/**
+ * Converts OSGB36 coordinates to WGS84 coordinates.
+ * OSGB36 is the latitude/longitutde system used by the United Kingdom Ordnance Survey.
+ * WGS84 is the latitude/longitutde system used by the iPhone.
+ *
+ * @param latitude the latitude in OSGB36 format
+ * @param longitude the longitude in OSGB36 format
+ * @param error pointer to an NSError object. This should be set to nil
+ * @return CBLatLon object containing the latitude and longitude in WGS84 format,
+ * or nil if an error occurred, in which case error will point to a valid
+ * NSError object with the error details.
+ */
 -(CBLatLon*)convertOSGB36toWGS84:(double)latitude longitude:(double)longitude error:(NSError **)error;
+
+/**
+ * Converts WGS84 coordinates to OSGB36 coordinates.
+ * OSGB36 is the latitude/longitutde system used by the United Kingdom Ordnance Survey.
+ * WGS84 is the latitude/longitutde system used by the iPhone.
+ *
+ * @param latitude the latitude in WGS84 format
+ * @param longitude the longitude in WGS84 format
+ * @param error pointer to an NSError object. This should be set to nil
+ * @return CBLatLon object containing the latitude and longitude in OSGB36 format,
+ * or nil if an error occurred, in which case error will point to a valid
+ * NSError object with the error details.
+ */
 -(CBLatLon*)convertWGS84toOSGB36:(double)latitude longitude:(double)longitude error:(NSError **)error;
+
+/**
+ * Converts an OSGB36 latitude and longitude to a United Kingdom Ordnance Survey grid reference.
+ *
+ * @param latitude the latitude in OSGB36 format
+ * @param longitude the longitude in OSGB36 format
+ * @param error pointer to an NSError object. This should be set to nil
+ * @return autoreleased NSString containing the eight figure grid reference, of the form:
+ * NS 5951 6547
+ * or nil if an error occurred, in which case error will point to a valid
+ * NSError object with the error details.
+ */
 -(NSString *)OSGridRefFromLatitude:(double)latitutde longitude:(double)longitude error:(NSError **)error;
 
 -(double)degreesToRadians:(double)degrees;
